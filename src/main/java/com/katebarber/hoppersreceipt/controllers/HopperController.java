c3d427b4eaa529c08c1127428b53d8f9b477d10e
KMBarber1 <katherine.m.barber@outlook.com>
2022-04-20 00:57:56 -0700

    initial submit

---
+++ src/main/java/com/katebarber/hoppersreceipt/controllers/HopperController.java
@@ -0,0 +1,30 @@
+package com.katebarber.hoppersreceipt.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HopperController {
+
+
+	@GetMapping("/")
+	public String index(Model model) {
+	        
+        String name = "Grace Hopper";
+        String itemName = "Copper wire";
+        double price = 5.25;
+        String description = "Metal strips, also an illustration of nanoseconds";
+        String vendor = "Little Things Corner Store";
+        
+        model.addAttribute("name", name);
+        model.addAttribute("itemName", itemName);
+        model.addAttribute("description", description);
+        model.addAttribute("price", price);
+        model.addAttribute("vendor", vendor);
+        
+	    return "index.jsp";
+	}
+
+}
